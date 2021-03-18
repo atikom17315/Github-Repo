c71bf6ad1f929e828737faf9b064c33748932bce
Atikom Tancharoen <atikom.apple@gmail.com>
2021-03-18 18:42:08 +0700

    Initial Xcode Project.

---
+++ Github Repo/Github Repo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Repo
+//
+//  Created by Atikom Tancharoen on 18/3/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
